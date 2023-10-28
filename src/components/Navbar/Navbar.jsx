dade3fc8c3d5c5eca431e717b6770c708aa86a1c
AshikRahmanDev <ashikrahman.dev@gmail.com>
2023-10-28 16:19:50 +0600

    navbar commented and comming soon texted added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -85,6 +85,8 @@ const Navbar = () => {
               <small>joseph@bartholomewdevelopment.com</small>
             </div>
           </div>
+          
+
           {/* address commented */}
           {/* <div className="flex items-center gap-2">
             <MdLocationOn className="text-primary text-4xl" />
@@ -104,6 +106,10 @@ const Navbar = () => {
             backgroundImage: `Url(${paper})`,
           }}
         >
+          {/* comming soon */}
+          <div className="bg-gray-200 h-[50px] w-full flex justify-center items-center">
+            <p className="font-cursive text-3xl text-green-700">comming soon...</p>
+          </div>
           {/* navbar */}
           {/* <div className="max-w-7xl w-full px-5 mx-auto md:flex items-center justify-between pt-5 pb-8">
             <ul className="hidden md:flex items-center gap-5 text-white2">
