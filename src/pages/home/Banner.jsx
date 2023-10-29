3e78c86920a549d572341b21af67beb588e7d40c
AshikRahmanDev <ashikrahman.dev@gmail.com>
2023-10-29 20:18:38 +0600

    banner carousel removed

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -1,23 +1,10 @@
 import slider1 from "../../assets/slider1.jpg";
-import slider2 from "../../assets/slider2.jpg";
-import slider3 from "../../assets/slider3.jpg";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectFade, Pagination, Autoplay } from "swiper/modules";
 import SliderText from "../../components/SliderText";
 
 const Banner = () => {
   return (
-    <Swiper
-      spaceBetween={30}
-      autoplay={{
-        delay: 10000,
-        disableOnInteraction: false,
-      }}
-      modules={[EffectFade, Pagination, Autoplay]}
-      className="mySwiper h-[85vh]"
-    >
-      <SwiperSlide>
-        <div className="relative">
+    <div className="h-[85vh]">
+     <div className="relative">
           <img
             className="brightness-50 h-[85vh] w-full object-cover"
             src={slider1}
@@ -25,28 +12,7 @@ const Banner = () => {
           />
           <SliderText />
         </div>
-      </SwiperSlide>
-      {/* <SwiperSlide>
-        <div className="relative">
-          <img
-            className="brightness-50 h-[85vh] w-full object-cover"
-            src={slider2}
-            alt="slider2"
-          />
-          <SliderText />
-        </div>
-      </SwiperSlide> */}
-      <SwiperSlide>
-        <div className="relative">
-          <img
-            className="brightness-50 h-[85vh] w-full object-cover"
-            src={slider3}
-            alt="slider3"
-          />
-          <SliderText />
-        </div>
-      </SwiperSlide>
-    </Swiper>
+    </div>
   );
 };
 
