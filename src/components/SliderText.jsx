5f8c58cb1fb8e7223a82c1b1c3abb7e3fc591c61
AshikRahmanDev <ashikrahman.dev@gmail.com>
2023-10-29 21:25:17 +0600

    banner modal form btn added

--- src/components/SliderText.jsx
+++ src/components/SliderText.jsx
@@ -1,11 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
-import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
-import { useSwiper } from "swiper/react";
-import SlideChangerArrow from "./SlideChangerArrow";
+import { Modal } from "../shared/Modal";
 
 const SliderText = () => {
-  const swiper = useSwiper();
+  const [isOpen,setIsOpen]=useState(true)
 
   return (
     <div className="w-full absolute top-1/2 md:left-1/2 left-5 transform md:-translate-x-1/2 md:px-10 -translate-y-1/2 text-white">
@@ -18,24 +16,20 @@ const SliderText = () => {
           <p className="text-[12px] my-5 md:w-3/4 w-[90%]">
             FARMS Systems is a holistic Farm And Ranch Management Software designed to streamline operations and maximize profitability in modern agriculture. With features ranging from comprehensive asset management and labor handling to intelligent accounting and automated Purchase Orders, FARMS Systems offers everything you need to run your farm or ranch efficiently. Exciting News: We're launching discounted beta subscriptions in Fall 2025! Take control of your agriculture business and seize this limited opportunity to watch your profits soar with FARMS Systems.
           </p>
-
-          <Button>Sign Up for Beta News</Button>
-        </div>
-        <div className="sm:flex flex-col gap-2 hidden">
-          <SlideChangerArrow
-            clickEvent={() => swiper.slidePrev()}
-            icon={<BsArrowLeft />}
-            hover={"group/leftArrow"}
-            afterHover={"group-hover/leftArrow:animate-fade-left"}
-          />
-          <SlideChangerArrow
-            clickEvent={() => swiper.slideNext()}
-            icon={<BsArrowRight />}
-            hover={"group/rightArrow"}
-            afterHover={"group-hover/rightArrow:animate-fade-right"}
-          />
+          <button 
+          className="btn" 
+          onClick={()=>{
+            setIsOpen(true)
+            if(isOpen){
+              document.getElementById('my_modal_1').showModal()
+            }
+          }}><Button>Sign Up for Beta News</Button>
+          </button>
         </div>
       </div>
+      {
+        isOpen && <Modal setIsOpen={setIsOpen} />
+      }
     </div>
   );
 };
